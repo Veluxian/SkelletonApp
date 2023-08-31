f4cd4dfdbf407b710ebeea42f2d79c4af48b009f
joaquin <joaquinosotelo@gmail.com>
2023-08-30 21:24:27 -0400

    cambios al login

--- SkelletonApp/src/app/login/login.page.ts
+++ SkelletonApp/src/app/login/login.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +7,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
+  login:any={
+    Usuario:"",
+    Contrasena:""
+  }
+  field:string="";
 
-  constructor() { }
+  constructor(public toastController: ToastController) { }
 
   ngOnInit() {
   }
 
+  ingresar(){
+    if(this.validateModel(this.login)){
+      this.presentToast("bienvenido");
+    }
+    else{
+      this.presentToast("falta: " + this.field);
+    }
+  }
+
+  validateModel(model:any){
+    for (var [key, value] of Object.entries(model)){
+      if(value == ""){
+        this.field=key;
+        return false;
+      }
+    }
+    return true;
+  }
+  async presentToast(message:string, duration?:number){
+    const toast = await this.toastController.create(
+      {
+        message:message,
+        duration:duration?duration: 2000
+      }
+    );
+    toast.present();
+  }
 }
