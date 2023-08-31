f49fb82301e616a38fa734ee160df92a231be750
joaquin <joaquinosotelo@gmail.com>
2023-08-30 21:58:53 -0400

    avance de la pagina

--- SkelletonApp/src/app/home/home.page.ts
+++ SkelletonApp/src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +7,40 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-  public alertButtons = ['yes'];
-  constructor() {}
+  usuario:String;
+  niveles:any[]=[
+    {id:1,nivel:"Media"},
+    {id:2,nivel:"Tecnico"},
+    {id:3,nivel:"Superior"}
+  ]
 
+  data:any={
+    nombre:"",
+    apellido:"",
+    educacion:"",
+    nacimiento:""
+  };
+  
+  constructor(public alertController:AlertController) {}
+
+  limpiar(){
+    for(var [key,value] of Object.entries(this.data)){
+      Object.defineProperty(this.data,key,{value:""})
+    }
+  }
+
+  mostrar(){
+    (this.data.nombre!="" && this.data.apellido!="") && 
+    this.presentAlert("usuario", "Su nombre es " + this.data.nombre + " " + this.data.apellido);
+  }
+
+  async presentAlert(titulo:string, message:string){
+    const alert= await this.alertController.create({
+      header: titulo,
+      message: message,
+      buttons: ['Listo']
+    });
+
+    await alert.present();
+  }
 }
