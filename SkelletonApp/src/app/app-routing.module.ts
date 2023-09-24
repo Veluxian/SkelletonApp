5b6489ca9c7dacd5923f7c2034e601ce4d272bcb
Joaquin Orellana <joaquinosotelo@gmail.com>
2023-09-24 19:07:14 -0300

    agregamos el page eror 404

--- SkelletonApp/src/app/app-routing.module.ts
+++ SkelletonApp/src/app/app-routing.module.ts
@@ -15,6 +15,11 @@ const routes: Routes = [
     path: 'login',
     loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
   },
+  {
+    path: '**',
+    loadChildren: () => import('./error404/error404.module').then( m => m.Error404PageModule)
+  },
+
 
 ];
 
